8811434b3bb69a764f3bd67cc970c942e5394bf0
maximkaby <drupalmasterpro@mail.ru>
2017-09-22 14:00:45 +0300

    Work

---
+++ mytest.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const check = require('./src/index.js');
+
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+check('((()))()', config1);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    console.log(bracketsConfig[0]);
+    //str = str.slice();
+    let brackets =str.split('');
+    let stack =[];
+    let head = -1;
+    let onDeleteBracket = false;
+
+    stack.push(brackets[0]);
+    head++;
+
+    for(let i = 1; i < brackets.length; i++){
+        stack.push(brackets[i]);
+        head++;
+        onDeleteBracket = false;
+        for( let arr of bracketsConfig){
+            if(stack[head - 1] == arr[0] &&
+                stack[head] == arr[1]){
+                onDeleteBracket = true;
+                break;
+            }
+        }
+
+        if(onDeleteBracket == true){
+            stack.pop();
+            stack.pop();
+            head-=2;
+        }
+
+    }
+    if(stack.length == 0)
+        return true;
+    return false;
 }
